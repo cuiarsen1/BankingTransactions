88ca91d71e1148bc2f0102a74dcd86f6921d55a3
Arsen Cui <ay4cui@uwaterloo.ca>
2025-03-28 15:26:28 -0400

    Initial commit for project setup

---
+++ src/test/java/com/example/bankingtransactions/BankingTransactionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankingtransactions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransactionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
